5d8ebb8ab347334aaed01943ab2a46c025513c48
Alimence <giranezaalmence@gmail.com>
2025-04-05 23:03:50 +0300

    chore: functioning login system

--- scripts.js
+++ scripts.js
@@ -1,16 +1,42 @@
+const users = [
+    {
+       username: 'admin',
+       password: 'admin123',
+       role: 'admin'
+    },
+    {
+        username: 'editor',
+       password: 'editor123',
+       role: 'editor'
+    }, 
+    {
+        username: 'viewer',
+       password: 'viewer123',
+       role: 'viewer'
+    }
+]
+
 const loginForm = document.querySelector('#loginForm')
 
 loginForm.addEventListener('submit', function(e) {
     e.preventDefault();
 
-     const username = document.querySelector('#userName').value;
-     const password = document.querySelector('#password').value;
+     const username = document.querySelector('#userName').value.trim();
+     const password = document.querySelector('#password').value.trim();
      const errorContainer = document.querySelector('#errorMessage');
+     const successContainer = document.querySelector('#successMessage');
 
-     if (!username.trim() || !password.trim()) {
+     if (!username || !password) {
         errorContainer.textContent = 'Please fill in all fields';
      } else {
         errorContainer.textContent = '';
-     }
-
+        const user = users.find(user => user.username === username && user.password === password);
+        if (user) {
+            errorContainer.textContent = '';
+            successContainer.textContent = `Welcome ${user.role}`;
+            //alert(`Welcome ${user.role}`);
+        } else {
+            errorContainer.textContent = 'Invalid username or password';
+        }
+    } 
 })
